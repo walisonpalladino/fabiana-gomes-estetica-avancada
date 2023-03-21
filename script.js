9dfd1099192745b507c0f5b03809e375059f0eec
WALISON TEIXEIRA GUALBERTO <walisonteixeira2011@gmail.com>
2023-03-21 19:35:23 -0300

    Ajustes gerais

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function showSlides() {
     slideIndex = 1
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 10000); // Change image every 5 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
 window.addEventListener("load", function() {
